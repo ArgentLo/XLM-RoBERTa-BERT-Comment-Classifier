cbbc697d026053b9a2c2826f69cdf9a5298f0e25
ArgentLo <argent.lo.hk@gmail.com>
2020-06-02 17:09:44 +0000

    test config

--- eval.py
+++ eval.py
@@ -34,6 +34,40 @@ def run(model_path):
     model.eval()
     print(">>> model loaded.")
 
+
+    print(">>> Start evaluation on raw test data ....")
+    df_test = pd.read_csv(config.TEST_DATA)
+    test_dataset = dataset.BERTDatasetTest(comment_text=df_test.content.values)
+    print(f"test size: {len(test_dataset)}")
+    test_data_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=config.TEST_BATCH_SIZE,
+        drop_last=False,
+        shuffle=False,
+        num_workers=config.TEST_WORKERS
+    )
+
+    with torch.no_grad():
+        fin_output_raw = []
+        for bi, d in tqdm(enumerate(test_data_loader), total=len(test_data_loader)):
+            ids = d["ids"]
+            mask = d["mask"]
+            token_type_ids = d["token_type_ids"]
+
+            ids = ids.to(device, dtype=torch.long)
+            mask = mask.to(device, dtype=torch.long)
+            token_type_ids = token_type_ids.to(device, dtype=torch.long)
+
+            outputs = model(
+                ids=ids,
+                mask=mask,
+                token_type_ids=token_type_ids
+            )
+
+            outputs_np = outputs.cpu().detach().numpy().tolist()
+            fin_output_raw.extend(outputs_np)
+
+
     print(">>> Start evaluation on EN 1 ....")
     df_en = pd.read_csv(config.TEST_CAMARON)
     test_dataset = dataset.BERTDatasetTest(comment_text=df_en.content_en.values)
@@ -99,15 +133,16 @@ def run(model_path):
             outputs_np = outputs.cpu().detach().numpy().tolist()
             fin_outputs_en2.extend(outputs_np)
 
-    return fin_outputs_en, fin_outputs_en2
+    return fin_output_raw, fin_outputs_en, fin_outputs_en2
 
 if __name__ == "__main__":
 
-    trained_model = "trainset_alex_1.bin"
+    trained_model = "trainset_alex_2.bin"
     model_path = "./checkpoints/" + trained_model
 
-    fin_outputs_en, fin_outputs_en2 = run(model_path)
-    fin_outputs_en = [item for sublist in fin_outputs_en for item in sublist]
+    fin_output_raw, fin_outputs_en, fin_outputs_en2 = run(model_path)
+    fin_output_raw  = [item for sublist in fin_output_raw for item in sublist]
+    fin_outputs_en  = [item for sublist in fin_outputs_en for item in sublist]
     fin_outputs_en2 = [item for sublist in fin_outputs_en2 for item in sublist]
 
     sample = pd.read_csv("../data/sample_submission.csv")
@@ -115,7 +150,7 @@ if __name__ == "__main__":
 
     # # sacle the predictions
     # sample = scale_min_max_submission(sample)
+    sample.to_csv(f"./checkpoints/{trained_model[:-4]}_eng_test.csv", index=False)
 
-    sample.to_csv(f"./checkpoints/{trained_model[:-4]}.csv", index=False)
-
-    
+    sample.loc[:, "toxic"] = np.array(fin_output_raw)
+    sample.to_csv(f"./checkpoints/{trained_model[:-4]}_raw_test.csv", index=False)
